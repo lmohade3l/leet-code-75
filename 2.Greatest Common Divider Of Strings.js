eef3f92d9d2a3c12343c3c3b51e9ed784d2997f8
lmohade3l <mohade3.2710@gmail.com>
2025-01-02 22:46:31 +0330

    day 2

---
+++ 2.Greatest Common Divider Of Strings.js
@@ -0,0 +1,13 @@
+/**
+ * For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+    Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+ */
+
+const gcdOfStrings = (str1, str2) => {
+  if (str1 + str2 !== str2 + str1) return "";
+
+  const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+
+  const gcdLength = gcd(str1.length, str2.length);
+  return str1.substring(0, gcdLength);
+};
