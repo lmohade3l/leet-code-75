763e0bea65e5cca5a9ed868e95a0dc35d3293a2b
lmohade3l <mohade3.2710@gmail.com>
2025-01-06 21:06:08 +0330

    day 6

--- 5.Reverse Vowels Of A String.js
+++ 5.Reverse Vowels Of A String.js
@@ -1,21 +1,27 @@
+/**
+ * 
+Given a string s, reverse only all the vowels in the string and return it.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+ */
+
 var reverseVowels = function (s) {
-    let vowels = []
-    let result = ''
+  let vowels = [];
+  let result = "";
 
-    for (let i = 0; i < s?.length; i++) {
-        if (['a', 'e', 'i', 'u', 'o', 'A', 'E', 'I', 'U', 'O'].includes(s[i])) vowels.push(s[i])
+  for (let i = 0; i < s?.length; i++) {
+    if (["a", "e", "i", "u", "o", "A", "E", "I", "U", "O"].includes(s[i]))
+      vowels.push(s[i]);
+  }
+
+  let j = vowels?.length - 1;
+  for (let i = 0; i < s?.length; i++) {
+    if (["a", "e", "i", "u", "o", "A", "E", "I", "U", "O"].includes(s[i])) {
+      result += vowels[j];
+      j--;
+    } else {
+      result += s[i];
     }
+  }
 
-    let j = vowels?.length - 1;
-    for (let i = 0; i < s?.length; i++) {
-        if (['a', 'e', 'i', 'u', 'o', 'A', 'E', 'I', 'U', 'O'].includes(s[i])) {
-            result += vowels[j];
-            j--;
-        } else {
-            result += s[i]
-        }
-    }
-
-    return result;
-
+  return result;
 };
