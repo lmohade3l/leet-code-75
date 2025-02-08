532293f953d26c67bce39221ec7ab397a8f895df
Mohadese Ghasemmehrabi <87853947+lmohade3l@users.noreply.github.com>
2025-02-08 23:44:30 +0330

    day 14

---
+++ 14.Maximum Average Subarray I.js
@@ -0,0 +1,19 @@
+var findMaxAverage = function(nums, k) {
+    if(nums?.length===1) return Number(nums[0].toFixed(5));
+
+    let left = 0;
+    let right = k-1;
+
+    let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+    let max = sum / k;
+
+    for (let i = k; i < nums.length; i++) {
+        sum = sum - nums[i - k] + nums[i];
+        max = Math.max(max, sum / k);
+    }
+    
+    return max;
+};
