8f3e1f3651fc820422274dba10d56a4c91fb976e
lmohade3l <mohade3.2710@gmail.com>
2025-03-22 17:52:53 +0330

    day 15

---
+++ 15.Maximum Number of Vowels in a Substring of Given Length.js
@@ -0,0 +1,29 @@
+var maxVowels = function (s , k) {
+    let m =0;
+    let n =k-1;
+    let count = 0;
+
+    for (let i=0; i<k; i++) {
+        if(["a" , "e" , "i" , "u" , "o"].includes(s[i])) count++;
+    }
+
+    m++;
+    n++;
+    let temp = count;
+
+    while(n < s.length) {
+        if(["a" , "e" , "i" , "u" , "o"].includes(s[m-1])) temp--;
+        if(["a" , "e" , "i" , "u" , "o"].includes(s[n])) temp++;
+
+        console.log({temp} , {count})
+
+        count = Math.max(temp , count);
+
+        m++;
+        n++;
+    }
+
+    return count;
+}
+
+console.log(maxVowels("leetcode" , 3))
