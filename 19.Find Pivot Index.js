d8974534149a92c4701dba91b6679a29e54ce55f
lmohade3l <mohade3.2710@gmail.com>
2025-03-27 00:14:17 +0330

    day 19 update

--- 19.Find Pivot Index.js
+++ 19.Find Pivot Index.js
@@ -1,3 +1,13 @@
 var pivotIndex = function(nums) {
-    
+    let rightSum = nums.reduce((arr , cur) => arr+cur,0)
+    let leftSum = 0;
+
+    for (let i=0; i<nums?.length; i++) {
+        rightSum = rightSum - nums[i];
+        if (rightSum===leftSum) return i;
+
+        leftSum = leftSum + nums[i]
+    }
+
+    return -1;
 };
