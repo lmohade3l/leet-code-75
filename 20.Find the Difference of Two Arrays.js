987cf9b1a681fdfd851d473abcbf1bf6868491cf
lmohade3l <mohade3.2710@gmail.com>
2025-03-27 16:07:20 +0330

    day 20 update

--- 20.Find the Difference of Two Arrays.js
+++ 20.Find the Difference of Two Arrays.js
@@ -1,17 +1,9 @@
 var findDifference = function (nums1, nums2) {
-    let arr1 = [];
-    let arr2 = [];
-  
-    let i1 = 0;
-    let i2 = 0;
-  
-    while (i1 < nums1.length || i2 < nums2.length) {
-      if (nums1[i1]!==undefined && !nums2.includes(nums1[i1]) && !arr1.includes(nums1[i1])) arr1.push(nums1[i1]);
-      if (nums2[i2]!==undefined && !nums1.includes(nums2[i2]) && !arr2.includes(nums2[i2])) arr2.push(nums2[i2]);
-  
-      i1++;
-      i2++;
-    }
-  
-    return [arr1, arr2];
-  };
+  const set1 = new Set(nums1);
+  const set2 = new Set(nums2);
+
+  const arr1 = [...set1].filter((num) => !set2.has(num));
+  const arr2 = [...set2].filter((num) => !set1.has(num));
+
+  return [arr1, arr2];
+};
