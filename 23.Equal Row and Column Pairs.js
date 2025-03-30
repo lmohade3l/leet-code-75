dc726ea251a96ad62ebc2f9012c38eee41bb68f2
lmohade3l <mohade3.2710@gmail.com>
2025-03-30 21:41:28 +0330

    day 23 update solution

--- 23.Equal Row and Column Pairs.js
+++ 23.Equal Row and Column Pairs.js
@@ -1,17 +1,44 @@
+// var equalPairs = function (grid) {
+//   let n = grid.length;
+
+//   let transposedGrid = grid[0]?.map((_, colIndex) =>
+//     grid.map((row) => row[colIndex])
+//   );
+
+//   let count = 0;
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//       if (JSON.stringify(grid[i]) === JSON.stringify(transposedGrid[j]))
+//         count++;
+//     }
+//   }
+//   return count;
+// };
+
 var equalPairs = function (grid) {
-  let n = grid.length;
+  let countPair = 0;
+  const hashArr = [];
 
-  let transposedGrid = grid[0]?.map((_, colIndex) =>
-    grid.map((row) => row[colIndex])
-  );
-
-  let count = 0;
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (JSON.stringify(grid[i]) === JSON.stringify(transposedGrid[j]))
-        count++;
+  for (let i = 0; i < grid.length; i++) {
+    let hash = 0;
+    for (let j = 0; j < grid.length; j++) {
+      hash = grid[i][j] + (hash << 6) + (hash << 16) - hash;
     }
+    hashArr.push(hash);
   }
-  return count;
-};
 
+  for (let i = 0; i < grid.length; i++) {
+    let hash = 0;
+    for (let j = 0; j < grid.length; j++) {
+      hash = grid[j][i] + (hash << 6) + (hash << 16) - hash;
+    }    
+
+    for (let k = 0; k < hashArr.length; k++) {
+        if (hash === hashArr[k]) {
+            countPair++;
+        }
+      }
+  }
+
+  return countPair;
+};
