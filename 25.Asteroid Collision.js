f39972af64ae7b20e0c992b0753f9a2b5774ff29
lmohade3l <mohade3.2710@gmail.com>
2025-04-01 22:49:23 +0330

    day 25: solution update

--- 25.Asteroid Collision.js
+++ 25.Asteroid Collision.js
@@ -1,4 +1,4 @@
-var asteroidCollision = function (asteroids) {
+var asteroidCollision1 = function (asteroids) {
   let stack = [...asteroids];
   let n = asteroids.length - 1;
 
@@ -6,7 +6,7 @@ var asteroidCollision = function (asteroids) {
     if (stack[n] * stack[n - 1] < 0) {
       const last = stack.pop();
       const onToLast = stack.pop();
-      console.log({stack} , {last} , {onToLast})
+      console.log({ stack }, { last }, { onToLast });
       if (Math.abs(last) > onToLast) {
         stack.push(last);
       } else if (Math.abs(last) < onToLast) {
@@ -15,11 +15,35 @@ var asteroidCollision = function (asteroids) {
         n--;
       }
     }
-    console.log({stack})
+    console.log({ stack });
     n--;
   }
 
   return stack;
 };
 
-console.log(asteroidCollision([10,2,-5]));
+var asteroidCollision = function (asteroids) {
+  const stack = [];
+
+  for (let i = 0; i < asteroids.length; i++) {
+    let a = asteroids[i];
+    if (a > 0) stack.push(a);
+    else {
+      //while there are positive values that have magnitude less than a's, pop them
+      while (
+        stack.length > 0 &&
+        stack[stack.length - 1] > 0 &&
+        stack[stack.length - 1] < Math.abs(a)
+      ) {
+        stack.pop();
+      }
+      //if all values have been poped
+      if (stack.length == 0 || stack[stack.length -1] < 0) stack.push(a);
+      else if (stack[stack.length - 1] == Math.abs(a)) stack.pop();
+    }
+  }
+
+  return stack
+};
+
+console.log(asteroidCollision([10, 2, -5]));
