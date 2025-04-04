523975faae27676aeecebbdb10f70219a9864c1a
lmohade3l <mohade3.2710@gmail.com>
2025-04-04 20:34:58 +0330

    day 27

---
+++ 27.Number of Recent Calls.js
@@ -0,0 +1,13 @@
+function RecentCounter() {
+  this.requests = [];
+}
+
+RecentCounter.prototype.ping = function (t) {
+  this.requests.push(t);
+
+  while (this.requests[0] < t - 3000) {
+    this.requests.shift();
+  }
+
+  return this.requests.length;
+};
