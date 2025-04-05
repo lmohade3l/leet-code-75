a236fa60946774f4958b5ee38df6055326a12c5c
lmohade3l <mohade3.2710@gmail.com>
2025-04-06 00:16:30 +0330

    day 28 solution update

--- 28.Dota2 Senate.js
+++ 28.Dota2 Senate.js
@@ -1,6 +1,19 @@
 var predictPartyVictory = function (senate) {
-    const stack = senate.split("")
-    while(stack.length > 0) {
-        
-    }
+  let radiantIndexes = [];
+  let direIndexes = [];
+
+  for (let i = 0; i < senate.length; i++) {
+    if (senate[i] == "R") radiantIndexes.push(i);
+    else direIndexes.push(i);
+  }
+
+  while (radiantIndexes.length > 0 && direIndexes.length > 0) {
+    const r = radiantIndexes.shift();
+    const d = direIndexes.shift();
+
+    if (r < d) radiantIndexes.push(senate.length + r);
+    else direIndexes.push(senate.length + d);
+  }
+
+  return radiantIndexes.length > 0 ? "Radiant" : "Dire";
 };
