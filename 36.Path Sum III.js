098c5b9fc06dcb4a8ef4140be133908ecf088d9b
lmohade3l <mohade3.2710@gmail.com>
2025-04-21 21:49:12 +0330

    day 36

--- 36.Path Sum III.js
+++ 36.Path Sum III.js
@@ -1,3 +1,26 @@
-var pathSum = function(root, targetSum) {
-    
+var pathSum = function (root, targetSum) {
+  if (!root) return 0;
+  let total = 0;
+
+  function dfs(node, currentSum) {
+    if (!node) return;
+
+    currentSum += node.val;
+    if (currentSum === targetSum) {
+      total++;
+    }
+    dfs(node.left, currentSum);
+    dfs(node.right, currentSum);
+  }
+
+  function exploreAllNodes(node) {
+    if (!node) return;
+    dfs(node, 0);
+
+    exploreAllNodes(node.left);
+    exploreAllNodes(node.right);
+  }
+
+  exploreAllNodes(root);
+  return total;
 };
