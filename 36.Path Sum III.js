a63ee1d16b87339f855c477a61ca8d325019c4e8
lmohade3l <mohade3.2710@gmail.com>
2025-04-23 13:36:13 +0330

    day 36 faster solution

--- 36.Path Sum III.js
+++ 36.Path Sum III.js
@@ -1,26 +1,50 @@
 var pathSum = function (root, targetSum) {
+  //   if (!root) return 0;
+  //   let total = 0;
+
+  //   function dfs(node, currentSum) {
+  //     if (!node) return;
+
+  //     currentSum += node.val;
+  //     if (currentSum === targetSum) {
+  //       total++;
+  //     }
+  //     dfs(node.left, currentSum);
+  //     dfs(node.right, currentSum);
+  //   }
+
+  //   function exploreAllNodes(node) {
+  //     if (!node) return;
+  //     dfs(node, 0);
+
+  //     exploreAllNodes(node.left);
+  //     exploreAllNodes(node.right);
+  //   }
+
+  //   exploreAllNodes(root);
+  //   return total;
+
   if (!root) return 0;
-  let total = 0;
+  const prefixSum = new Map();
+
+  // we have one path that sum of the values from root to the end of the path is 0 (the root itself)
+  prefixSum.set(0, 1);
+
+  let totalPaths = 0;
 
   function dfs(node, currentSum) {
     if (!node) return;
-
     currentSum += node.val;
-    if (currentSum === targetSum) {
-      total++;
-    }
+
+    totalPaths += prefixSum.get(currentSum - targetSum) || 0;
+    prefixSum.set(currentSum, prefixSum.get(currentSum) || 0 || 0);
+
     dfs(node.left, currentSum);
     dfs(node.right, currentSum);
+
+    // back track
+    prefixSum.set(currentSum, prefixSum.get(currentSum) - 1);
   }
 
-  function exploreAllNodes(node) {
-    if (!node) return;
-    dfs(node, 0);
-
-    exploreAllNodes(node.left);
-    exploreAllNodes(node.right);
-  }
-
-  exploreAllNodes(root);
-  return total;
+  dfs(root, 0);
 };
